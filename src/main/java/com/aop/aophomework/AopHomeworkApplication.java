1aedf4b3dbdf1eebaed6f0736d7301ad810476f3
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-27 17:29:07 +0400

    Initial commit

---
+++ src/main/java/com/aop/aophomework/AopHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.aop.aophomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopHomeworkApplication.class, args);
+    }
+
+}
